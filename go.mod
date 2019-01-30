4fc66b4451df74809b86147883e460389afbccb2
huangshaokun <645636204@qq.com>
2019-01-30 17:51:52 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,26 @@
+module git.algor.tech/huangsk/funnel
+
+require (
+	github.com/Shopify/sarama v1.17.0
+	github.com/Shopify/toxiproxy v2.1.3+incompatible // indirect
+	github.com/aws/aws-sdk-go v1.16.23
+	github.com/eapache/go-resiliency v1.1.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180703130627-040cc1a32f57 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/fortytw2/leaktest v1.2.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/go-ini/ini v1.25.4 // indirect
+	github.com/influxdata/influxdb v1.7.3
+	github.com/influxdata/platform v0.0.0-20190117200541-d500d3cf5589 // indirect
+	github.com/mailru/easyjson v0.0.0-20180723221831-d5012789d665 // indirect
+	github.com/nats-io/go-nats v1.7.0
+	github.com/nats-io/nkeys v0.0.2 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20180503174638-e2704e165165 // indirect
+	github.com/smartystreets/gunit v0.0.0-20180314194857-6f0d6275bdcd // indirect
+	github.com/spf13/viper v1.2.1
+	github.com/xlab/handysort v0.0.0-20150421192137-fb3537ed64a1 // indirect
+	golang.org/x/net v0.0.0-20181023162649-9b4f9f5ad519
+	gopkg.in/olivere/elastic.v5 v5.0.70
+	gopkg.in/redis.v5 v5.2.9
+	vbom.ml/util v0.0.0-20170409195630-256737ac55c4
+)
---
+++ outputs/kafka.go
@@ -0,0 +1,105 @@
+// +build !disablekafka
+
+package outputs
+
+// This is kafka output writer
+import (
+	"log/syslog"
+	"time"
+
+	"git.algor.tech/huangsk/funnel"
+	"github.com/Shopify/sarama"
+	"github.com/spf13/viper"
+)
+
+// Registering the constructor function
+func init() {
+	funnel.RegisterNewWriter("kafka", newKafkaOutput)
+}
+
+func newKafkaOutput(v *viper.Viper, logger *syslog.Writer) (funnel.OutputWriter, error) {
+	// Setting up the kafka config
+	cfg := sarama.NewConfig()
+	cfg.Producer.Compression = sarama.CompressionGZIP
+	if v.IsSet("target.flush_frequency_secs") {
+		cfg.Producer.Flush.Frequency = time.Duration(v.GetInt("target.flush_frequency_secs")) * time.Second
+	}
+	if v.IsSet("target.batch_size") {
+		cfg.Producer.Flush.Messages = v.GetInt("target.batch_size")
+	}
+	if v.IsSet("target.max_retries") {
+		cfg.Producer.Retry.Max = v.GetInt("target.max_retries")
+	}
+	if v.IsSet("target.write_timeout_secs") {
+		cfg.Net.WriteTimeout = time.Duration(v.GetInt("target.write_timeout_secs")) * time.Second
+	}
+	cfg.Producer.Return.Successes = false
+	cfg.Producer.Return.Errors = true
+	cfg.Producer.RequiredAcks = sarama.WaitForLocal
+	cfg.ClientID = v.GetString("target.clientID")
+
+	brokers := v.GetStringSlice("target.brokers")
+	topic := v.GetString("target.topic")
+	p, err := sarama.NewAsyncProducer(brokers, cfg)
+	if err != nil {
+		return nil, err
+	}
+	// Creating the struct
+	k := &kafkaOutput{
+		producer: p,
+		topic:    topic,
+		logger:   logger,
+		msgChan:  make(chan *sarama.ProducerMessage),
+		done:     make(chan struct{}),
+	}
+	// Starting the producer loop
+	go k.startProducerLoop()
+	return k, nil
+}
+
+// kafkaOutput contains the stuff to write to kafka
+type kafkaOutput struct {
+	producer sarama.AsyncProducer
+	topic    string
+	logger   *syslog.Writer
+	msgChan  chan *sarama.ProducerMessage
+	done     chan struct{}
+}
+
+// Implementing the OutputWriter interface
+
+func (k *kafkaOutput) Write(p []byte) (n int, err error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+	// Send a msg to the channel
+	k.msgChan <- &sarama.ProducerMessage{
+		Topic: k.topic,
+		Value: sarama.StringEncoder(string(p[:len(p)-1])), // stripping off the trailing newline
+	}
+	return len(p), nil
+}
+
+func (k *kafkaOutput) Flush() error {
+	return nil
+}
+
+func (k *kafkaOutput) Close() error {
+	// Send done signal to exit from goroutine
+	k.done <- struct{}{}
+	// Close producer
+	return k.producer.Close()
+}
+
+func (k *kafkaOutput) startProducerLoop() {
+	for {
+		select {
+		case msg := <-k.msgChan:
+			k.producer.Input() <- msg
+		case err := <-k.producer.Errors():
+			k.logger.Err(err.Error())
+		case <-k.done:
+			return
+		}
+	}
+}
